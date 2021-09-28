a2efd1aa493f7070e8fe0d2e015d3a21361b76de
BatselierBryan <bryan.batselier@student.hogent.be>
2021-09-28 10:29:37 +0200

    Test commit

--- TruitjesVerkoop/TruitjesVerkoop/Model/Bestellingen.cs
+++ TruitjesVerkoop/TruitjesVerkoop/Model/Bestellingen.cs
@@ -8,5 +8,10 @@ namespace BusinessLayer.Model
 {
     class Bestellingen
     {
+        //Constructor
+        public int Bestellingsnummer { get; private set;}
+        public DateTime Datum { get; private set;}
+        public bool isBetaald { get; private set;}
+        public double VerkoopPrijs { get; private set;}
     }
 }
